92bcfbb9e24f5bc9603771347104e51c8c22bf41
Sergei Dmitrijev <mr.dmit@gmail.com>
2019-08-22 12:06:56 +0300

    Beginning working on app

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -8,11 +8,30 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
 export default (state = [], action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case CREATE_NEW_ORDER:
+            return [
+                ...state,
+                {
+                    id: action.payload.id,
+                    ingredients: [],
+                    position: 'clients',
+                    recipe: [...action.payload.recipe]
+                }
+        ]
+        case MOVE_ORDER_NEXT:
+            return state;
+        case MOVE_ORDER_BACK:
+            return state;
+        case ADD_INGREDIENT:
+            return state;
+
+        default:
+            return state;
+    }
 };
 
-export const getOrdersFor = (state, position) =>
-  state.orders.filter(order => order.position === position);
+export const getOrdersFor = (state, position) => {
+    state.orders.filter(order => order.position === position);
+
+}
